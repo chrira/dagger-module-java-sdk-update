ff9af07931521839c087d9efec657e2bdef4be8b
Christoph Raaflaub <raaflaub@puzzle.ch>
2024-03-20 17:19:53 +0100

    prepare java 17 with dagger container

--- java-sdk/dagger/main.go
+++ java-sdk/dagger/main.go
@@ -18,12 +18,18 @@ import (
 	"context"
 )
 
-type JavaSdk struct{}
+type JavaSdk struct {
+	Ctr *Container
+}
 
 // Returns a container that echoes whatever string argument is provided
-func (m *JavaSdk) ContainerEcho(stringArg string) *Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+func (m *JavaSdk) ContainerEcho(ctx context.Context, stringArg string) (string, error){
+	return dag.Container().
+				From("registry.access.redhat.com/ubi9/openjdk-17:1.18-1").
+				WithExec([]string{"gzip", stringArg}).
+				Stdout(ctx)
 }
+
 
 // Returns lines that match a pattern in the files of the provided Directory
 func (m *JavaSdk) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
@@ -33,4 +39,43 @@ func (m *JavaSdk) GrepDir(ctx context.Context, directoryArg *Directory, pattern 
 		WithWorkdir("/mnt").
 		WithExec([]string{"grep", "-R", pattern, "."}).
 		Stdout(ctx)
+}
+
+func (m *JavaSdk) GetJDK() *JavaSdk {
+	m.Ctr = dag.Container().
+		From("registry.access.redhat.com/ubi9/openjdk-17:1.18-1").
+			WithUser("root").
+			WithExec([]string{"microdnf", "-y", "install", "gzip"}).
+			WithUser("185")
+	return m
+}
+
+func (m *JavaSdk) InstallDagger() *JavaSdk {
+	m.Ctr = m.Ctr.
+		WithUser("root").
+		WithWorkdir("/usr/local").
+		WithExec([]string{"curl", "-L", "-o", "install.sh", "https://dl.dagger.io/dagger/install.sh"}).
+		WithExec([]string{"chmod", "+x", "./install.sh"}).
+		WithExec([]string{"./install.sh"}).
+		WithUser("185")
+	//	WithExec([]string{"DAGGER_VERSION=0.9.7", "./install.sh"})
+	return m
+
+
+// cd /usr/local
+// curl -L https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.9.7 sh
+
+
+}
+
+func (m *JavaSdk) DaggerVersion(ctx context.Context, container *Container) (string, error){
+	return container.
+				WithExec([]string{"dagger", "version"}).
+				Stdout(ctx)
+}
+
+func (m *JavaSdk) CI(ctx context.Context) (string, error){
+	return m.GetJDK().InstallDagger().Ctr.
+				WithExec([]string{"/usr/local/bin/dagger", "version"}).
+				Stdout(ctx)
 }
